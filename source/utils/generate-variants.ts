df4c981a9198ecc17b9a72a60b08549dda065919
Joris Mertz <info@jorismertz.nl>
2024-05-06 18:51:36 +0200

    clean up

--- source/utils/generate-variants.ts
+++ source/utils/generate-variants.ts
@@ -52,28 +52,29 @@ function generateVariant(
 		`$${variantKeys.indexOf(variant.key as any) + 1}`,
 	);
 
-	console.log("Output", config.output + "/" + filename);
-
 	fs.mkdirSync(config.output, { recursive: true });
 	fs.writeFileSync(path.join(config.output, filename), result, "utf8");
 }
 
 export const generateVariants = (config: Config) => {
 	const isDir = fs.lstatSync(config.template).isDirectory();
-	console.log("isDir", isDir);
 	const extension = path.extname(config.template);
 
 	for (const variant of variantKeys) {
 		const currentVariant = variants[variant];
+
 		if (isDir) {
 			fs.mkdirSync(config.output + "/" + currentVariant.key, {
 				recursive: true,
 			});
-			for (const file of fs.readdirSync(config.template)) {
+
+			const templateFiles = fs.readdirSync(config.template);
+			for (const file of templateFiles) {
 				const template = fs.readFileSync(
 					path.join(config.template, file),
 					"utf8",
 				);
+
 				generateVariant(
 					currentVariant,
 					template,
@@ -90,48 +91,5 @@ export const generateVariants = (config: Config) => {
 				config,
 			);
 		}
-		// const currentVariant = variants[variant];
-		// const description =
-		// 	"All natural pine, faux fur and a bit of soho vibes for the classy minimalist";
-		// const type = variant === "dawn" ? "light" : "dark";
-		//
-		// let result = template;
-		//
-		// for (const role of roleKeys) {
-		// 	const currentColor = roleColors[role][variant];
-		//
-		// 	if ("alpha" in currentColor) {
-		// 		// Replace alpha colors
-		// 		result = result.replaceAll(
-		// 			`${config.prefix}${role}Alpha`,
-		// 			formatColor(currentColor.alpha, config.format, config.stripSpaces),
-		// 		);
-		// 	}
-		//
-		// 	// Replace colors
-		// 	result = result.replaceAll(
-		// 		`${config.prefix}${role}`,
-		// 		formatColor(currentColor, config.format, config.stripSpaces),
-		// 	);
-		// }
-		//
-		// // Replace built-in values
-		// result = result.replaceAll(`${config.prefix}id`, currentVariant.id);
-		// result = result.replaceAll(`${config.prefix}name`, currentVariant.name);
-		// result = result.replaceAll(`${config.prefix}description`, description);
-		// result = result.replaceAll(`${config.prefix}type`, type);
-		//
-		// // Replace custom values
-		// result = result.replaceAll(
-		// 	/\$\((.*?)\|(.*?)\|(.*?)\)/gm,
-		// 	`$${variantKeys.indexOf(variant) + 1}`,
-		// );
-		//
-		// fs.mkdirSync(config.output, { recursive: true });
-		// fs.writeFileSync(
-		// 	`${config.output}/${currentVariant.id}${extension}`,
-		// 	result,
-		// 	"utf8",
-		// );
 	}
 };
